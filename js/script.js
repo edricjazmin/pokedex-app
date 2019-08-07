b341d8214d0a19efec5a8c1e5b1826d7a30c6b6b
edricjazmin <51036316+edricjazmin@users.noreply.github.com>
2019-08-07 00:44:32 -0400

    Updated javascript file
    Changed js/script.js to include loop and conditional.
    
    Starting to update styling and css.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-alert('Hello World');
+var repository = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass ', ' poison']
+  },
+  {
+    name: 'Charmander',
+    height: 1.7,
+    type: 'fire'
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    type: 'water'
+  }
+];
+
+for (var i = 0; i < repository.length; i++) {
+  if (repository[i].height > 1) {
+    document.write(
+      repository[i].name + ' (height: ' + repository[i].height + 'm) - Wow, that\'s big! ' + '<br>')
+  } else {
+    document.write(
+      repository[i].name + ' (height: ' + repository[i].height + 'm) ' + '<br>');
+  }
+}
