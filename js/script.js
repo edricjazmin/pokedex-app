a3dc92a98f097347ded7c434c4ba63d2d3d2162f
edricjazmin <51036316+edricjazmin@users.noreply.github.com>
2019-08-14 17:33:18 -0400

    Updated .js file
    added 'add', 'getAll', and 'return' functions to pokemonRepository;

--- js/script.js
+++ js/script.js
@@ -1,42 +1,30 @@
-var repository = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass ', ' poison']
-  },
-  {
-    name: 'Charmander',
-    height: 0.6,
-    type: 'fire'
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    type: 'water'
-  },
-  {
-    name: 'Caterpie',
-    height: 0.3,
-    type: 'bug'
-  },
-  {
-    name: 'Weedle',
-    height: 0.3,
-    type: ['bug', 'poison']
-  },
-  {
-    name: 'Pidgey',
-    height: 0.3,
-    type: ['normal', 'flying']
-  },
-  {
-    name: 'Rattata',
-    height: 0.3,
-    type: 'normal'
-  }
-];
+var pokemonRepository = (function() {
+  var repository = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, type: 'fire'},
+    {name: 'Squirtle', height: 0.5, type: 'water'},
+    {name: 'Caterpie', height: 0.3, type: 'bug'},
+    {name: 'Weedle', height: 0.3, type: ['bug', 'poison']},
+    {name: 'Pidgey', height: 0.3, type: ['normal', 'flying']},
+    {name: 'Rattata', height: 0.3, type: 'normal'}
+  ];
 
-repository.forEach(function(currentPokemon){
+  function add(pokemon) {
+    return repository.push(pokemon);
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  return {
+    add: add,
+    getAll: getAll();
+  };
+
+} ) ();
+
+pokemonRepository.getAll().forEach(function(currentPokemon){
   if (currentPokemon.height > 0.6) {
     document.write(
       '<p>' + currentPokemon.name + ' (height: ' + currentPokemon.height + 'm) - Wow, that\'s big! ' + '</p> <hr>')
